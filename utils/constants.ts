ec26b88449445e852e1cf76f1b3eb32a27c26acf
Adam Grund <adam.grund@gmail.com>
2024-04-09 13:09:51 +0200

    Paas icon (#17)
    * renamimg
    
    * add paas icon in mpa emails
    
    * remove .react-email from gitignore
    
    * add .react-email to .gitignore
    
    * svg to png change

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,13 @@
 export const cdnImageAddress =
   "https://cdn-api.bratislava.sk/tax-personal-estates/images";
+
+export const HeaderIconVariant = {
+  city: {
+    address: "/bratislava-castle-medium.png",
+    altName: "Bratislava Castle",
+  },
+  paas: {
+    address: "/email-header-logo-paas.png",
+    altName: "Paas logo",
+  },
+};
