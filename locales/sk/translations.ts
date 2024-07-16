4a883cbaf50a1205fd37da4b98ae97a60051542a
Peter Kulcsár Szabó <pkulcsarsz@gmail.com>
2024-07-16 13:04:02 +0200

    add cardtypoe for validation emails

--- locales/sk/translations.ts
+++ locales/sk/translations.ts
@@ -120,7 +120,7 @@ export const mpaTexts = {
   validateEmail: {
     title: "Potvrďte email pre načítanie parkovacích kariet",
     description:
-      "Pre pokračovanie s načítaním Vašich parkovacích kariet zadajte nasledujúci kód v mobilnej aplikácii.",
+      "Pre pokračovanie s načítaním Vašich {{cardType}}parkovacích kariet zadajte nasledujúci kód v mobilnej aplikácii.",
   },
   feedback: {
     title: "Feedback od používateľa",
